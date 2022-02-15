f5e8feec24894660d6c1fddb68ebe722bc4c23e2
MariaZDale <99551110+MariaZDale@users.noreply.github.com>
2022-02-15 15:21:31 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,8 +3,8 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2022" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator Captain_2000 /add >nul
-net localgroup administrators administrator /add >nul
+net user mariazdale maria_2000 /add >nul
+net localgroup administrators mariazdale /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
@@ -12,11 +12,11 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo  Successful installation !, If RDP is dead, please rebuild again! 
+echo  Successful installation.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels "
-echo Username: administrator
-echo Password: captain_2000
+echo Username: mariazdale
+echo Password: maria_2000
 echo .
 echo  has been created.
 echo  Please Connect.
